361ec2a6d6421bcbd063c3a7d247917e32ca281b
Dominique Ruizendaal <dominiqueruizendaal@iMac.local>
2026-01-09 11:00:05 +0100

    Fix server to respond correctly on Railway

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
   res.end("ClarityFlow draait");
 });
 
-server.listen(PORT, () => {
-  console.log("ClarityFlow draait op poort " + PORT);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`ClarityFlow draait op poort ${PORT}`);
 });
